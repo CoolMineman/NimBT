429864cd33ae343f80c98b8910971d04f7e1823d
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-06-21 22:19:50 -0500

    make cooler

--- NBTParser/parsenbt.nim
+++ NBTParser/parsenbt.nim
@@ -11,20 +11,33 @@ var indexlocation = 0
 type
     NBTKind = enum  # the different node types
         tagString,
-        tagCompound
+        tagCompound,
+        tagList,
+        tagByte,
+        tagShort,
+        tagInt,
+        tagLong
     NBTNode = ref NBTNodeObj
     NBTNodeObj = object
         name: string
         case kind: NBTKind  # the ``kind`` field is the discriminator
         of tagString:
-            value: string
-        of tagCompound:
+            stringvalue: string
+        of tagCompound, tagList:
             entries: seq[NBTNode]
+        of tagByte:
+            int8value: int8
+        of tagShort:
+            int16value: int16
+        of tagInt:
+            int32value: int32
+        of tagLong:
+            int64value: int64
 
 proc readByte(): uint8 =
     if (indexlocation >= buffersize):
         indexlocation = 0
-        bytesread = readBytes(f, inputbuffer, 0, buffersize) # todo hardcoding 0 may be broken
+        bytesread = readBytes(f, inputbuffer, 0, buffersize) # todo hardcoding 0 ~~may be~~ is broken
     result = inputbuffer[indexlocation]
     indexlocation += 1
     #echo indexlocation
@@ -36,29 +49,113 @@ proc readString(): string =
     for i in 1..int32(length):
         result = result & char(readByte())
 
-proc readTagString: NBTNode =
-    result = NBTNode(kind: tagString)
-    result.name = readString()
-    result.value = readString()
+proc readInt8(): int8 =
+    return cast[int8](readByte())
 
-proc readTagCompound(): NBTNode =
-    result = NBTNode(kind: tagCompound)
-    result.name = readString()
+proc readInt16(): int16 =
+    var a: uint16 = 0
+    a += readByte()
+    a = a shl 8
+    a += readByte()
+    return cast[int16](a)
+
+proc readInt32(): int32 =
+    var a: uint32 = 0
+    a += readByte()
+    a = a shl 8
+    a += readByte()
+    a = a shl 8
+    a += readByte()
+    a = a shl 8
+    a += readByte()
+    return cast[int32](a)
+
+proc readInt64(): int64 =
+    var a: uint64 = 0
+    a += readByte()
+    a = a shl 8
+    a += readByte()
+    a = a shl 8
+    a += readByte()
+    a = a shl 8
+    a += readByte()
+    a = a shl 8
+    a += readByte()
+    a = a shl 8
+    a += readByte()
+    a = a shl 8
+    a += readByte()
+    a = a shl 8
+    a += readByte()
+    return cast[int64](a)
+
+proc readNode(tagID: uint8): NBTNode =
+    case tagID
+    of 8:
+        result = NBTNode(kind: tagString)
+        result.stringvalue = readString()
+    of 1:
+        result = NBTNode(kind: tagByte)
+        result.int8value = readInt8()
+    of 2:
+        result = NBTNode(kind: tagShort)
+        result.int16value = readInt16()
+    of 3:
+        result = NBTNode(kind: tagInt)
+        result.int32value = readInt32()
+    of 4:
+        result = NBTNode(kind: tagLong)
+        result.int64value = readInt64()
+    else:
+        echo "no"
+    
+
+proc readTagCompoundOrList(named: bool, isList: bool): NBTNode =
+    if isList:
+        result = NBTNode(kind: tagList)
+    else:
+        result = NBTNode(kind: tagCompound)
+    if named:
+        result.name = readString()
     result.entries = newSeq[NBTNode](0)
-    while true:
-        case readByte()
-        of 8:
-            var a: NBTNode = readTagString()
-            result.entries.add(a)
-        of 0:
-            break
-        else:
-            echo "bad nbt type"
+    if isList:
+        let tagID = readByte()
+        let length = readInt32()
+        if length > 0:
+            for _ in 1..length:
+                case tagID
+                of 1, 2, 3, 4, 8:
+                    result.entries.add(readNode(tagID))
+                of 9:
+                    result.entries.add(readTagCompoundOrList(false, true))
+                of 10:
+                    result.entries.add(readTagCompoundOrList(false, false))
+                else:
+                    echo "no"
+    else:
+        while true:
+            var a = readByte()
+            case a
+            of 1, 2, 3, 4, 8:
+                var name = readString()
+                var b = readNode(a)
+                b.name = name
+                result.entries.add(b)
+            of 9:
+                result.entries.add(readTagCompoundOrList(true, true))
+            of 10:
+                result.entries.add(readTagCompoundOrList(true, false))
+            of 0:
+                break
+            else:
+                echo "bad nbt type"
+                break
+    
 
-if open(f, "test.nbt"):
+if open(f, "test3.nbt"):
     bytesread = readBytes(f, inputbuffer, 0, buffersize)
     if (bytesread > 0 and readByte() == 10):
-        let a = readTagCompound()
+        let a = readTagCompoundOrList(named = true, isList = false)
         echo %*a
     else:
         echo "bad nbt file"
